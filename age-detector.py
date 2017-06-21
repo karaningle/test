b04df23d67f7824a718b75a7319cff3867de4a2e
Ingle, Karan (US - Mumbai) <kaingle@deloitte.com>
2017-06-21 18:23:35 +0530

    Age detector added

---
+++ age-detector.py
@@ -0,0 +1,10 @@
+print "Magic Age Detector"
+name = raw_input("Enter your name: ")
+animal = raw_input("Enter your favourite animal: ")
+sex = raw_input("Enter your gender [m/f]: ")
+if name == "Karan":
+	print "Karan, my master, You are God!"
+elif sex == "f":
+	print name + ", I do not tell a girl's age. It is impolite!"
+else:
+	print name + ", Don't waste my time."
